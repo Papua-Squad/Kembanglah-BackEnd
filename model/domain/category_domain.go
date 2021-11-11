1845a989cfeac7eeb798ef559b312107a187aca3
Aura Qolbani <aura.qolbani@gmail.com>
2021-11-11 15:40:10 +0700

    add register

--- model/domain/category_domain.go
+++ model/domain/category_domain.go
@@ -4,4 +4,6 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
+	Name string
+	Type string
 }
--- model/domain/product_domain.go
+++ model/domain/product_domain.go
@@ -4,4 +4,10 @@ import "gorm.io/gorm"
 
 type Product struct {
 	gorm.Model
+	Name        string
+	Price       int
+	Stock       int
+	Weight      int
+	Type        int
+	Description string
 }
