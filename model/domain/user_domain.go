7958fcc4408a589cea3f58076c7355e588a98a81
iniudin <sysfdn@gmail.com>
2021-12-01 19:22:34 +0700

    nice try lagi

--- model/domain/user_domain.go
+++ model/domain/user_domain.go
@@ -7,8 +7,8 @@ import (
 type User struct {
 	gorm.Model
 	FullName string
-	Email    string
-	Username string
+	Email    string `gorm:"unique"`
+	Username string `gorm:"unique"`
 	Password string
 	Role     string
 }
